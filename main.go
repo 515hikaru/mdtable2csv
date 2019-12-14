4f7f5ab66106d4f951bb5d1d8912948bf3ad53f7
515hikaru <12kojima.takahiro@gmail.com>
2019-12-14 15:07:33 +0900

    Extract text from tree

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
 
 import (
-	"os"
+	"fmt"
 
 	"github.com/gomarkdown/markdown/ast"
 	"github.com/gomarkdown/markdown/parser"
 )
+
+func getTextRaw(node ast.Node) []string {
+	switch node := node.(type) {
+	case *ast.TableHeader:
+		raw := node.GetChildren()[0]
+		return getTextRaw(raw)
+	case *ast.TableBody:
+		raw := node.GetChildren()[0]
+		return getTextRaw(raw)
+	case *ast.TableRow:
+		var ss []string
+		for _, c := range node.GetChildren() {
+			leaf := c.GetChildren()[0].AsLeaf()
+			ss = append(ss, string(leaf.Literal))
+		}
+		return ss
+	default:
+		return []string{}
+	}
+}
+
+func getAllTableCell(node ast.Node) {
+	for _, child := range node.GetChildren() {
+		for _, c := range child.GetChildren() {
+			fmt.Println(getTextRaw(c))
+		}
+	}
+}
 
 func main() {
 	input := `
@@ -18,5 +46,5 @@ func main() {
 
 	parser := parser.New()
 	output := parser.Parse(inputByte)
-	ast.Print(os.Stdout, output)
+	getAllTableCell(output)
 }
